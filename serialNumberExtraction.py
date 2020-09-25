47f66bde69d7506cf589ac6281247aff1bded9bb
HYEJINKIM <un123k@naver.com>
2020-09-26 04:17:42 +0900

    rename variable, correct typo, etc.

--- serialNumberExtraction.py
+++ serialNumberExtraction.py
@@ -90,33 +90,32 @@ class MyApp(QWidget):
         i = 1   # 분할해서 내보낼 색인어 파일의 넘버
         
         article_df = pd.read_csv(articlefilename, encoding = 'UTF-8-sig') # 기사 취합본 파일 불러오기
-        guideword_df = pd.read_csv(guidewordfilename, encoding = 'UTF-8-sig')    # 색인어 파일 불러오기
-        guideword_list = list(guideword_df) # 색인어를 리스트로
-        guideword_df = None     # 색인어 불러온 데이터 프레임 비우기
+        guideword_list = pd.read_csv(guidewordfilename, encoding = 'UTF-8-sig')    # 색인어 파일 불러오기
+        guideword_list = list(guideword_list) # 데이터프레임에서 리스트로
         
         serialnumber_df = pd.DataFrame()   # 최종적으로 파일로 내보낼 색인어+일련번호가 저장될 데이터프레임
         
         for word_list in guideword_list:
-            serialnumber = list()  # 색인어를 colname으로 갖는, 추출된 일련번호를 저장할 변수
+            serialnumber_list = list()  # 최종적으로 색인어를 colname으로 갖는, 추출된 일련번호 시리즈를 저장할 변수
             word_list = str.split(word_list, '/')   # 슬래시를 기준으로 실제 검색할 단어 리스트 생성
             
             for word in word_list:
                 if '(' in word:    # 실제 검색할 단어(word)에서 불필요한 부분 제거 -> (~~~)부분 제거
                     word = word[0 : word.find('(') :]
             
-                tmp_list = list()   # word를 포함한 기사의 일련번호를 저장할 리스트
+                tmp_list = list()   # word를 포함한 기사의 일련번호를 저장할 임시 리스트
                 is_contained = article_df['기사명'].str.contains(word) # 기사명에 word가 포함된 행을 추출하기 위해 인덱스 정보 저장
-                tmp_list = article_df[is_contained] # 기사명에 word가 포함된 행만 tmp_list에 저장
+                tmp_list = article_df[is_contained] # 기사명에 word가 포함된 행만 임시 리스트에 저장
             
                 if len(tmp_list) == 0:  # word를 포함하는 기사가 없을 경우
                     string = word + '없음'
-                    serialnumber.append(string)
+                    serialnumber_list.append(string)
                     continue
-                serialnumber.extend(list(tmp_list['일련번호'])) # 추출된 데이터프레임에서 '일련번호' 열만 리스트로 만들어 serialnumber에 저장
+                serialnumber_list.extend(list(tmp_list['일련번호'])) # 추출된 데이터프레임에서 '일련번호' 열만 리스트로 만들어 일련번호 리스트에 추가
             
-            guideword = str.join('ㆍ', word_list)
-            serialnumber = pd.Series(serialnumber, name = guideword)   # 추출된 일련번호 리스트를 시리즈로
-            serialnumber_df = pd.concat([serialnumber_df, serialnumber], axis = 1)   # 색인어 데이터프레임에 추출된 일련번호를 한 열에 추가
+            guideword = str.join('ㆍ', word_list)    # /(슬래시)로 나눈 단어들을 ㆍ(중점)으로 이어줌 == 원래 색인어 파일에 있던 색인어의 형태로 돌려줌
+            serialnumber_list = pd.Series(serialnumber_list, name = guideword)   # 추출된 일련번호 리스트를 시리즈로
+            serialnumber_df = pd.concat([serialnumber_df, serialnumber_list], axis = 1)   # 일련번호 데이터프레임에 추출된 일련번호 한 열 추가
             
             if serialnumber_df.count().sum() > 5000:  # 데이터 수가 5000을 넘어가면 내보낸 후 이어서 추출
                 save_path = path + '색인어' + str(i) + '.csv'
